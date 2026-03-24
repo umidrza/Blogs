fcce5263e3d2564a144e1410ac239fb8cc706adc
umidrza <umidrza4@gmail.com>
2026-03-24 21:13:32 +0400

    add user getById

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,33 +1,49 @@
-const bcrypt = require('bcrypt')
-const usersRouter = require('express').Router()
-const User = require('../models/user')
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
 
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-    .populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  });
 
-  response.json(users)
-})
+  response.json(users);
+});
 
-usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+usersRouter.get("/:id", async (request, response) => {
+  const user = await User.findById(request.params.id);
 
-  if (password.length < 6){
-    return response.status(400).json({ error: 'password must be at least 6 characters long' })
+  if (user) {
+    response.json(user);
+  } else {
+    response.status(404).end();
+  }
+});
+
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+
+  if (password.length < 6) {
+    return response
+      .status(400)
+      .json({ error: "password must be at least 6 characters long" });
   }
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
 
   const user = new User({
     username: username.toLowerCase(),
     name,
     passwordHash,
-  })
+  });
 
-  const savedUser = await user.save()
+  const savedUser = await user.save();
 
-  response.status(201).json(savedUser)
-})
+  response.status(201).json(savedUser);
+});
 
-module.exports = usersRouter
+module.exports = usersRouter;
